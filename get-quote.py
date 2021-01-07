a97de00f83c5bea06e5ef183923357e0f8a65d56
vineetpandey <vineetpandeyalld@gmail.com>
2021-01-07 06:42:47 -0500

    Worked on few extra recommendations

--- get-quote.py
+++ get-quote.py
@@ -6,11 +6,25 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  
-  print(quotes[rnd])
-  
+
+  # 1 & 4. Added quotes programmatically
+  f = open("quotes.txt", "a+")
+  f.writelines("Let's try out adding a new line." + str(rnd) + "\n")
+  f.close()
+  # Read the quotes
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+  # 2. Printed random no. of quotes
+  # 3. removed that extra line(newline) while printing
+  for i in range(0, rnd):
+    print(quotes[i].rstrip(), end='')
+    
+  print()
+
 if __name__== "__main__":
   primary()
